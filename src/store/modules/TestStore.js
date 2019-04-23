201ca24c12b8c74350af715f0abb158e71491756
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2019-04-23 17:06:34 +0200

    working on store

--- src/store/modules/TestStore.js
+++ src/store/modules/TestStore.js
@@ -2,15 +2,22 @@
 export default {
 
   state: {
-    message: 'this is my first state!'
+    message: 'this is my first state!',
+    myVariable: 0
   },
   getters: {
-
+    someFilter: state => {
+      return state.myVariable[0]
+    }
   },
   mutations: {
-
+    addToArray (state, value) {
+      state.myVariable.push(value)
+    }
   },
   actions: {
-
+    addToArray (context) {
+      context.commit('addToArray')
+    }
   }
 }

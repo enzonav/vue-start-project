26da7ecb4b68b7832fa5242489e83623c58d4b55
Vincenzo Navarra <vincenzo.navarra@usr.root.jus>
2019-07-09 16:29:05 +0200

    added vuetify and removed bootstrap packages

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './router/index.js'
 import store from './store/index.js'
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
 /* creating an event bus with no rendere vue instance */
